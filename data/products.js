304770611879ad0d8139afdb6988b48de862a492
eraykaymaz <eray.kaymaz@gmail.com>
2025-11-15 19:50:37 +0300

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,332 +1,332 @@
 window.PRODUCTS = {
   "CAT002": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT003": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
   ],
   "CAT004": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT005": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT006": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
   "CAT007": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/carbon8"
   ],
   "CAT008": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT009": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT010": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT011": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT012": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT013": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT014": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT015": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT016": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ds2-desk-stand"
   ],
   "CAT017": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
   ],
   "CAT018": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT019": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT020": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
   ],
   "CAT021": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT022": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/keylab-essential-mk3-50"
   ],
   "CAT023": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/carbon9"
   ],
   "CAT024": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT025": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT026": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT027": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT028": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/d510mx-175334610812390"
   ],
   "CAT029": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/dn370-equalizer"
   ],
   "CAT030": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT031": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT032": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ds2-desk-stand"
   ],
   "CAT033": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT034": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT035": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
   ],
   "CAT036": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
   ],
   "CAT037": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT038": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/keylab-essential-mk3-51"
   ],
   "CAT039": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/carbon10"
   ],
   "CAT040": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT041": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT042": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT043": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT044": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/d510mx-175334610812390"
   ],
   "CAT045": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/dn370-equalizer"
   ],
   "CAT046": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT047": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT048": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ds2-desk-stand"
   ],
   "CAT049": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
   ],
   "CAT050": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT051": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT052": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
   ],
   "CAT053": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/studimon-8"
   ],
   "CAT054": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT055": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/carbon11"
   ],
   "CAT056": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT057": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT058": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT059": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT060": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT061": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/dn370-equalizer"
   ],
   "CAT062": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT063": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT064": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ds2-desk-stand"
   ],
   "CAT065": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
   ],
   "CAT066": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT067": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
   ],
   "CAT068": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
   ],
   "CAT069": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT070": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/keylab-essential-mk3-53"
   ],
   "CAT071": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/carbon12"
   ],
   "CAT072": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT073": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ast-halo-reflection-filter-ve-vokal-panel"
   ],
   "CAT074": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT075": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT076": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/d510mx-175334610812390"
   ],
   "CAT077": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/dn370-equalizer"
   ],
   "CAT078": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT079": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT080": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ds2-desk-stand"
   ],
   "CAT081": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
   ],
   "CAT082": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT083": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
   ],
   "CAT084": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
   ],
   "CAT085": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/studimon-10"
   ],
   "CAT086": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/keylab-essential-mk3-54"
   ],
   "CAT087": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/carbon13"
   ],
   "CAT088": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT089": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT090": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT091": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT092": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/d510mx-175334610812390"
   ],
   "CAT093": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/dn370-equalizer"
   ],
   "CAT094": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT095": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT096": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/ds2-desk-stand"
   ],
   "CAT097": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT098": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/zg01-175559837212430"
   ],
   "CAT099": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
   ],
   "CAT100": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
   ],
   "CAT101": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT102": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/keylab-essential-mk3-55"
   ],
   "CAT103": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/carbon14"
   ],
   "CAT104": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT105": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT106": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT107": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
   ],
   "CAT108": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/d510mx-175334610812390"
   ],
   "CAT109": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/ssl-2-mkii-ses-karti"
   ],
   "CAT110": [
-    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
+    "https://www.sescibaba.com/go-solo"
   ],
   "CAT111": [
-    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
+    "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ]
 };
