7f915dd6639edd204835012be8d8096b1bf7a44d
eraykaymaz <eray.kaymaz@gmail.com>
2025-11-15 18:20:49 +0300

    Update products.js

--- data/products.js
+++ data/products.js
@@ -31,235 +31,235 @@ window.PRODUCTS =
     "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT011": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT012": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT013": [
-    "https://www.sescibaba.com/dn370-equalizer"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT014": [
-    "https://www.sescibaba.com/go-solo"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT015": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
+    "https://www.sescibaba.com/studimon-5"
   ],
   "CAT016": [
-    "https://www.sescibaba.com/ds2-desk-stand"
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
   "CAT017": [
-    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
+    "https://www.sescibaba.com/carbon8"
   ],
   "CAT018": [
-    "https://www.sescibaba.com/zg01-175559837212430"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT019": [
-    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT020": [
-    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT021": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT022": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT023": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT024": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT025": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/studimon-5"
   ],
   "CAT026": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
   "CAT027": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
+    "https://www.sescibaba.com/carbon8"
   ],
   "CAT028": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT029": [
-    "https://www.sescibaba.com/dn370-equalizer"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT030": [
-    "https://www.sescibaba.com/go-solo"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT031": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT032": [
-    "https://www.sescibaba.com/ds2-desk-stand"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT033": [
-    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT034": [
-    "https://www.sescibaba.com/zg01-175559837212430"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT035": [
-    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
+    "https://www.sescibaba.com/studimon-5"
   ],
   "CAT036": [
-    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
   "CAT037": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/carbon8"
   ],
   "CAT038": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT039": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT040": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT041": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT042": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT043": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT044": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT045": [
-    "https://www.sescibaba.com/dn370-equalizer"
+    "https://www.sescibaba.com/studimon-5"
   ],
   "CAT046": [
-    "https://www.sescibaba.com/go-solo"
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
   "CAT047": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
+    "https://www.sescibaba.com/carbon8"
   ],
   "CAT048": [
-    "https://www.sescibaba.com/ds2-desk-stand"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT049": [
-    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT050": [
-    "https://www.sescibaba.com/zg01-175559837212430"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT051": [
-    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT052": [
-    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT053": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT054": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
-  ],
-  "CAT055": [
-    "https://www.sescibaba.com/carbon8"
-  ],
-  "CAT056": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
-  ],
-  "CAT057": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
-  ],
-  "CAT058": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
-  ],
-  "CAT059": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
-  ],
-  "CAT060": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
-  ],
-  "CAT061": [
-    "https://www.sescibaba.com/dn370-equalizer"
-  ],
-  "CAT062": [
-    "https://www.sescibaba.com/go-solo"
-  ],
-  "CAT063": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
-  ],
-  "CAT064": [
-    "https://www.sescibaba.com/ds2-desk-stand"
-  ],
-  "CAT065": [
-    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
-  ],
-  "CAT066": [
-    "https://www.sescibaba.com/zg01-175559837212430"
-  ],
-  "CAT067": [
-    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
-  ],
-  "CAT068": [
-    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
-  ],
-  "CAT069": [
     "https://www.sescibaba.com/studimon-5"
   ],
-  "CAT070": [
+  "CAT055": [
     "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
-  "CAT071": [
+  "CAT056": [
     "https://www.sescibaba.com/carbon8"
   ],
-  "CAT072": [
+  "CAT057": [
     "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
-  "CAT073": [
+  "CAT058": [
     "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
+  "CAT059": [
+    "https://www.sescibaba.com/element-shock-mount"
+  ],
+  "CAT060": [
+    "https://www.sescibaba.com/starlight-clip"
+  ],
+  "CAT061": [
+    "https://www.sescibaba.com/starlight-windscreen"
+  ],
+  "CAT062": [
+    "https://www.sescibaba.com/stealth-stand-mount"
+  ],
+  "CAT063": [
+    "https://www.sescibaba.com/studimon-5"
+  ],
+  "CAT064": [
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
+  ],
+  "CAT065": [
+    "https://www.sescibaba.com/carbon8"
+  ],
+  "CAT066": [
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
+  ],
+  "CAT067": [
+    "https://www.sescibaba.com/element-shock-mount"
+  ],
+  "CAT068": [
+    "https://www.sescibaba.com/starlight-clip"
+  ],
+  "CAT069": [
+    "https://www.sescibaba.com/starlight-windscreen"
+  ],
+  "CAT070": [
+    "https://www.sescibaba.com/stealth-stand-mount"
+  ],
+  "CAT071": [
+    "https://www.sescibaba.com/studimon-5"
+  ],
+  "CAT072": [
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
+  ],
+  "CAT073": [
+    "https://www.sescibaba.com/carbon8"
+  ],
   "CAT074": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT075": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT076": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT077": [
     "https://www.sescibaba.com/dn370-equalizer"
   ],
   "CAT078": [
-    "https://www.sescibaba.com/go-solo"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT079": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT080": [
-    "https://www.sescibaba.com/ds2-desk-stand"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT081": [
-    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT082": [
-    "https://www.sescibaba.com/zg01-175559837212430"
-  ],
-  "CAT083": [
-    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
-  ],
-  "CAT084": [
-    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
-  ],
-  "CAT085": [
     "https://www.sescibaba.com/studimon-5"
   ],
-  "CAT086": [
+  "CAT083": [
     "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
-  "CAT087": [
+  "CAT084": [
     "https://www.sescibaba.com/carbon8"
+  ],
+  "CAT085": [
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
+  ],
+  "CAT086": [
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+  ],
+  "CAT087": [
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT088": [
     "https://www.sescibaba.com/32classic-ms-mix-strip"
@@ -286,49 +286,49 @@ window.PRODUCTS =
     "https://www.sescibaba.com/xd80usb-dijital-bateri"
   ],
   "CAT096": [
-    "https://www.sescibaba.com/au-am100-k2-maonocaster-podcast-solo-studio"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT097": [
-    "https://www.sescibaba.com/zg01-175559837212430"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT098": [
-    "https://www.sescibaba.com/ml-1a-modelleme-mikrofonu"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT099": [
-    "https://www.sescibaba.com/g101-yuksek-kalite-2-kanal-hifi-studyo-kulakliklari-icin-rca-stereo-masaustu-kulaklik-amplifikatoru"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT100": [
     "https://www.sescibaba.com/studimon-5"
   ],
   "CAT101": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/element-shock-mount"
   ],
   "CAT102": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/starlight-clip"
   ],
   "CAT103": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/starlight-windscreen"
   ],
   "CAT104": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/stealth-stand-mount"
   ],
   "CAT105": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/studimon-5"
   ],
   "CAT106": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
+    "https://www.sescibaba.com/keylab-essential-mk3-49"
   ],
   "CAT107": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
+    "https://www.sescibaba.com/carbon8"
   ],
   "CAT108": [
-    "https://www.sescibaba.com/dn370-equalizer"
+    "https://www.sescibaba.com/32classic-ms-mix-strip"
   ],
   "CAT109": [
-    "https://www.sescibaba.com/go-solo"
+    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
   ],
   "CAT110": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
+    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
   ],
   "CAT111": [
     "https://www.sescibaba.com/ds2-desk-stand"
