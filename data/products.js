0efd82c4a998d28ba6af01d3defc4b2d22f55cb8
eraykaymaz <eray.kaymaz@gmail.com>
2025-11-15 18:24:34 +0300

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,336 +1,336 @@
 window.PRODUCTS =
 {
   "CAT001": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT002": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT003": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT004": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT005": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT006": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT007": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT008": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT009": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT010": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT011": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT012": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT013": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT014": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT015": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT016": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT017": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT018": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT019": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT020": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT021": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT022": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT023": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT024": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT025": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT026": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT027": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT028": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT029": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT030": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT031": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT032": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT033": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT034": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT035": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT036": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT037": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT038": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT039": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT040": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT041": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT042": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT043": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT044": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT045": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT046": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT047": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT048": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT049": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT050": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT051": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT052": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT053": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT054": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT055": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT056": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT057": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT058": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT059": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT060": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT061": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT062": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT063": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT064": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT065": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT066": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT067": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT068": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT069": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT070": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT071": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT072": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT073": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT074": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT075": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT076": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT077": [
-    "https://www.sescibaba.com/dn370-equalizer"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT078": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT079": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT080": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT081": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT082": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT083": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT084": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT085": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT086": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT087": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT088": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT089": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT090": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT091": [
-    "https://www.sescibaba.com/sat4-tripodlu-dikdortgen-345-mikrofon-izolasyon-paneli"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT092": [
-    "https://www.sescibaba.com/d510mx-175334610812390"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT093": [
-    "https://www.sescibaba.com/dn370-equalizer"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT094": [
-    "https://www.sescibaba.com/go-solo"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT095": [
-    "https://www.sescibaba.com/xd80usb-dijital-bateri"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT096": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT097": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT098": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT099": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT100": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT101": [
-    "https://www.sescibaba.com/element-shock-mount"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT102": [
-    "https://www.sescibaba.com/starlight-clip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT103": [
-    "https://www.sescibaba.com/starlight-windscreen"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT104": [
-    "https://www.sescibaba.com/stealth-stand-mount"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT105": [
-    "https://www.sescibaba.com/studimon-5"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT106": [
-    "https://www.sescibaba.com/keylab-essential-mk3-49"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT107": [
-    "https://www.sescibaba.com/carbon8"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT108": [
-    "https://www.sescibaba.com/32classic-ms-mix-strip"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT109": [
-    "https://www.sescibaba.com/akai-apc64-ableton-live-kontrol-unitesi-sequencer"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ],
   "CAT110": [
-    "https://www.sescibaba.com/vms-plugin-radio-france-signature-exp-pack"
+    "https://www.sescibaba.com/boom-dahili-dsp-2x2-usb-c-ses-karti-171594783311308"
   ],
   "CAT111": [
-    "https://www.sescibaba.com/ds2-desk-stand"
+    "https://www.sescibaba.com/spirit-kondenser-mikrofon-paketi-siyah"
   ]
 }
